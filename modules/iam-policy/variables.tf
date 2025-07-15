06fe24988f613c54580f93668e6e99cf174abb1c
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-15 18:03:02 +0530

    Update variables.tf

--- modules/iam-policy/variables.tf
+++ modules/iam-policy/variables.tf
@@ -1,1 +1,20 @@
-variable "project" { type = string }
+variable "project" {
+  type        = string
+  description = "Logical project name used for tagging and resource names (e.g., lambda-image-resize)"
+}
+
+variable "source_bucket_arn" {
+  type        = string
+  description = "ARN of the uploads S3 bucket (without trailing /*)"
+}
+
+variable "dest_bucket_arn" {
+  type        = string
+  description = "ARN of the resized S3 bucket (without trailing /*)"
+}
+
+variable "sns_topic_arn" {
+  type        = string
+  description = "ARN of the SNS topic for notifications"
+}
+
