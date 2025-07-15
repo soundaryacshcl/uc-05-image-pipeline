74e625d2fd307c64c384f95c42a6a585e100e989
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-15 18:08:35 +0530

    Update main.tf

--- modules/iam-policy/main.tf
+++ modules/iam-policy/main.tf
@@ -1,83 +1,37 @@
-locals {
-  name_prefix = "${var.project}-${terraform.workspace}"
-}
-
-########################
-#  Trust Policy (STS)  #
-########################
-
-data "aws_iam_policy_document" "assume_lambda" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-############################
-#  IAM Role for the Lambda #
-############################
-
 resource "aws_iam_role" "lambda" {
-  name               = "${local.name_prefix}-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_lambda.json
+  name = "${var.project}-lambda-role"
 
-  tags = {
-    Project     = var.project
-    Environment = terraform.workspace
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = { Project = var.project }
 }
 
-#########################################
-#  Least‑Privilege Inline Policy JSON   #
-#########################################
+resource "aws_iam_role_policy" "all_access" {
+  name = "${var.project}-lambda-all"
+  role = aws_iam_role.lambda.id
 
-data "aws_iam_policy_document" "lambda_policy" {
-
-  # S3 object‑level access (read from uploads, write to resized)
-  statement {
-    sid       = "S3ObjectAccess"
-    effect    = "Allow"
-    actions   = [
-      "s3:GetObject",
-      "s3:PutObject"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*",
+          "lambda:*",
+          "sns:*",
+          "logs:*"
+        ]
+        Resource = "*"
+      }
     ]
-    resources = [
-      "${var.source_bucket_arn}/*",
-      "${var.dest_bucket_arn}/*"
-    ]
-  }
-
-  # Publish success/failure notifications
-  statement {
-    sid       = "SNSPublish"
-    effect    = "Allow"
-    actions   = ["sns:Publish"]
-    resources = [var.sns_topic_arn]
-  }
-
-  # CloudWatch Logs for Lambda executions
-  statement {
-    sid       = "CloudWatchLogs"
-    effect    = "Allow"
-    actions   = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
-#########################################
-#  Attach the inline policy to the role #
-#########################################
-
-resource "aws_iam_role_policy" "lambda" {
-  name   = "${local.name_prefix}-lambda-policy"
-  role   = aws_iam_role.lambda.id
-  policy = data.aws_iam_policy_document.lambda_policy.json
+  })
 }
