f8ec628dc576d3163a33e344f83e7ce1a4ee8484
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-15 15:51:02 +0530

    Add files via upload

--- modules/iam-policy/main.tf
+++ modules/iam-policy/main.tf
@@ -1,1 +1,37 @@
+resource "aws_iam_role" "lambda" {
+  name = "${var.project}-lambda-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = { Project = var.project }
+}
+
+resource "aws_iam_role_policy" "all_access" {
+  name = "${var.project}-lambda-all"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*",
+          "lambda:*",
+          "sns:*",
+          "logs:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
